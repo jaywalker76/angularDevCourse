50fb2d301b04a53a6834c49c35a560a0678c27e0
Julio Costa <juliocosta1@gmail.com>
2016-10-31 20:05:56 +0000

    Display repo information

--- githubSearch/app/components/profile.component.ts
+++ githubSearch/app/components/profile.component.ts
@@ -10,11 +10,18 @@ export class ProfileComponent {
     // bring data into template
 
     user: any[];
+    repos: any[];
 
     constructor(private _githubService: GithubService){
         this._githubService.getUser().subscribe(user => {
             // makes info accessible in html template
             this.user = user;
+            //console.log(user);
+        });
+
+        this._githubService.getRepos().subscribe(repos => {
+            // makes info accessible in html template
+            this.repos = repos;
             //console.log(user);
         });
     }
