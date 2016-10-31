ed78bd7fc3aa21da3a73187b898a822dbe7a75b9
Julio Costa <juliocosta1@gmail.com>
2016-10-31 19:11:55 +0000

    Testing getUser functionality;

---
+++ githubSearch/app/services/github.service.ts
@@ -0,0 +1,21 @@
+
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+    private username:string;
+    private client_id='b14aab4b556d09ad3ccd';
+    private client_secret='51ec1ab39a8954ff767807f7beaea382732b6506';
+
+    constructor(private _http: Http){
+        console.log('Github Service Ready...');
+        this.username = 'jaywalker76';
+    }
+
+    getUser(){
+        return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+}
